data-respons-solutions/kernel-lm-nxp

drivers/misc: Add pwm and function mux
Add modulated PWM for Linkbox ECG
Add pin function mux.

drivers/misc/dt-function-mux.c
@@ -1,0 +1,204 @@
+/*
+ * Simple PWM buzzer
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/pinctrl/pinctrl.h>
+
+
+#define MAX_HANDLES 2
+struct fmux_data {
+	struct device *dev;
+	const char* names[MAX_HANDLES];
+	int num_handles;
+	struct pinctrl *pinc;
+	struct pinctrl_state *pstate[MAX_HANDLES];
+	int active;
+};
+
+static DEFINE_MUTEX(sysfs_lock);
+
+
+static int fmux_reparse(struct fmux_data *priv)
+{
+	int ret;
+	int n;
+	ret = of_property_count_strings(priv->dev->of_node, "pinctrl-names");
+	if ( ret < 0 || ret > MAX_HANDLES) {
+		dev_err(priv->dev, "Wrong number of mux options or bad name string [%d]\n", ret);
+		return -EINVAL;
+	}
+
+	priv->num_handles = ret;
+	for (n=0; n < priv->num_handles; n++) {
+		ret = of_property_read_string_index(priv->dev->of_node, "pinctrl-names", n, &priv->names[n]);
+	}
+	return ret;
+}
+
+static int fmux_match(struct fmux_data *priv, const char* s)
+{
+	int n;
+	for (n=0; n < priv->num_handles; n++) {
+		if (strcmp(priv->names[n], s) == 0)
+			return n;
+	}
+	return -EINVAL;
+}
+
+static ssize_t fmux_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fmux_data *priv = dev_get_drvdata(dev);
+	ssize_t status=0;
+	mutex_lock(&sysfs_lock);
+	status = sprintf(buf, priv->names[priv->active]);
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static ssize_t fmux_opt_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fmux_data *priv = dev_get_drvdata(dev);
+	ssize_t status=0;
+	int n;
+	mutex_lock(&sysfs_lock);
+	for (n=0; n < priv->num_handles; n++)
+		status += sprintf(buf+status, "%s ", priv->names[n]);
+	status += sprintf(buf+status, "\n");
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static ssize_t fmux_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t sz)
+{
+	struct fmux_data *priv = dev_get_drvdata(dev);
+	size_t status = -EINVAL;
+	char sbuf[40];
+	int index;
+	mutex_lock(&sysfs_lock);
+	strncpy(sbuf, buf, min((int)sz, 39));
+	sbuf[sz] = '\0';
+
+	if ((index = fmux_match(priv, sbuf)) >= 0) {
+		if (index == priv->active)
+			status = sz;
+		else {
+			status = pinctrl_select_state(priv->pinc, priv->pstate[index]);
+			if (status < 0) {
+				dev_err(priv->dev, "Unable to select state %s\n", priv->names[index]);
+			}
+			else {
+				priv->active = index;
+				status = sz;
+			}
+		}
+	}
+	else {
+		dev_err(priv->dev, "Invalid selection %s\n", sbuf);
+	}
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static DEVICE_ATTR(select, 0644, fmux_show, fmux_store);
+static DEVICE_ATTR(possible, 0444, fmux_opt_show, NULL);
+
+
+static int fmux_probe(struct platform_device *pdev)
+{
+	struct fmux_data *priv;
+	int ret;
+	struct device_node *np = pdev->dev.of_node;
+
+	int status, n;
+
+	if (!np) {
+		dev_err(&pdev->dev, "No DT node found\n");
+		return -EINVAL;
+	}
+
+	priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv) {
+		dev_err(&pdev->dev, "no memory for state\n");
+		return -ENOMEM;
+	}
+
+	priv->dev = &pdev->dev;
+	platform_set_drvdata(pdev, priv);
+
+	fmux_reparse(priv);
+	if (priv->num_handles < 2) {
+		dev_err(priv->dev, "Too few [%d] handles\n", priv->num_handles);
+		status = -EINVAL;
+		goto deref;
+	}
+	priv->pinc = devm_pinctrl_get(priv->dev);
+	if ( IS_ERR(priv->pinc))
+	{
+		status = -ENODEV;
+		goto deref;
+	}
+	for (n=0; n < priv->num_handles; n++) {
+		priv->pstate[n] = pinctrl_lookup_state(priv->pinc, priv->names[n]);
+	}
+	mutex_lock(&sysfs_lock);
+	ret = pinctrl_select_state(priv->pinc, priv->pstate[0]);
+	if (ret < 0) {
+		dev_err(priv->dev, "Unable to select state %s\n", priv->names[0]);
+		status = ret;
+		mutex_unlock(&sysfs_lock);
+		goto deref;
+	}
+	priv->active = 0;
+	mutex_unlock(&sysfs_lock);
+
+	ret = sysfs_create_file(&pdev->dev.kobj, &dev_attr_select.attr);
+	ret = sysfs_create_file(&pdev->dev.kobj, &dev_attr_possible.attr);
+
+	return 0;
+deref:
+	return status;
+}
+
+static int fmux_remove(struct platform_device *pdev)
+{
+	sysfs_remove_file(&pdev->dev.kobj, &dev_attr_select.attr);
+	sysfs_remove_file(&pdev->dev.kobj, &dev_attr_possible.attr);
+	return 0;
+}
+
+static struct of_device_id fmux_of_match[] = {
+	{
+		.compatible = "datarespons,function-mux",
+	},
+	{}
+};
+
+MODULE_DEVICE_TABLE(of, fmux_of_match);
+
+static struct platform_driver fmux_driver = {
+	.driver =
+	{
+		.name = "function-mux",
+		.owner = THIS_MODULE,
+		.of_match_table = fmux_of_match,
+	},
+	.probe = fmux_probe,
+	.remove = fmux_remove,
+};
+
+module_platform_driver(fmux_driver);
+
+MODULE_DESCRIPTION("DT based function pinmux");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:function-mux");
+MODULE_AUTHOR("Hans Christian Lonstad <hcl@datarespons.no>");

drivers/misc/modulated_pwm.c
@@ -1,0 +1,147 @@
+/*
+ * Simple PWM buzzer
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/fb.h>
+#include <linux/backlight.h>
+#include <linux/err.h>
+#include <linux/pwm.h>
+#include <linux/slab.h>
+
+struct modulated_pwm_data {
+	struct pwm_device *pwm;
+	struct device *dev;
+	unsigned int pwmId;
+	struct pwm_state state;
+	int onValue;	/* dutycycle as onValue/256 */
+};
+
+static DEFINE_MUTEX(sysfs_lock);
+
+static ssize_t modulated_pwm_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+
+	struct modulated_pwm_data *pb = dev_get_drvdata(dev);
+	ssize_t status;
+	mutex_lock(&sysfs_lock);
+	status = sprintf(buf, "%d\n", pb->onValue);
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static ssize_t modulated_pwm_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t sz)
+{
+	struct modulated_pwm_data *pb = dev_get_drvdata(dev);
+	size_t status = sz;
+	int ret;
+	u8 val;
+
+	mutex_lock(&sysfs_lock);
+	ret = kstrtou8(buf, 10, &val);
+	if (ret < 0) {
+		status = ret;
+		goto exit;
+	}
+
+	pb->state.enabled = val == 0 ? false : true;
+	pwm_set_relative_duty_cycle(&pb->state, val, 255);
+
+	if ((ret = pwm_apply_state(pb->pwm, &pb->state)) < 0) {
+		dev_err(dev, "%s: Could not configure PWM", __func__);
+		status = ret;
+		goto exit;
+	} else {
+		pb->onValue = val;
+	}
+
+exit:
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static DEVICE_ATTR(period, 0644, modulated_pwm_show, modulated_pwm_store);
+
+static int modulated_pwm_probe(struct platform_device *pdev)
+{
+	struct modulated_pwm_data *pb;
+	int ret;
+	struct device_node *np = pdev->dev.of_node;
+	u32 period;
+	bool inverted = of_property_read_bool(np, "inverted");
+	if (of_property_read_u32(np, "period", &period))
+		period = 0;
+	else
+		dev_info(&pdev->dev, "Got period of %d ns from DT\n", period);
+
+	pb = devm_kzalloc(&pdev->dev, sizeof(*pb), GFP_KERNEL);
+	if (!pb) {
+		dev_err(&pdev->dev, "no memory for state\n");
+		return -ENOMEM;
+	}
+
+	if (!np) {
+		dev_err(&pdev->dev, "No DT node found\n");
+		return -EINVAL;
+	}
+
+	pb->dev = &pdev->dev;
+	pb->onValue = 0;
+	pb->state.duty_cycle = 0;
+	pb->pwm = devm_of_pwm_get(&pdev->dev, np, NULL);
+	if (IS_ERR(pb->pwm)) {
+		dev_err(&pdev->dev, "unable to request PWM\n");
+		return PTR_ERR(pb->pwm);
+	}
+	pwm_init_state(pb->pwm, &pb->state);
+	dev_dbg(&pdev->dev, "%s: got pwm %d, period %d ns, duty %d ns\n", __func__, pb->pwmId,
+				pb->state.period, pb->state.duty_cycle);
+	pb->state.polarity = inverted ? PWM_POLARITY_INVERSED : PWM_POLARITY_NORMAL;
+	if (period)
+		pb->state.period = period;
+	pwm_apply_state(pb->pwm, &pb->state);
+
+	ret = sysfs_create_file(&pdev->dev.kobj, &dev_attr_period.attr);
+	platform_set_drvdata(pdev, pb);
+	return 0;
+}
+
+static int modulated_pwm_remove(struct platform_device *pdev)
+{
+	struct backlight_device *bl = platform_get_drvdata(pdev);
+	struct modulated_pwm_data *pb = dev_get_drvdata(&bl->dev);
+	sysfs_remove_file(&pdev->dev.kobj, &dev_attr_period.attr);
+	pwm_disable(pb->pwm);
+	return 0;
+}
+
+static struct of_device_id modulated_pwm_of_match[] = {
+	{
+		.compatible = "modulated-pwm",
+	},
+	{}
+};
+
+MODULE_DEVICE_TABLE(of, modulated_pwm_of_match);
+
+static struct platform_driver modulated_pwm_driver = {
+	.driver =
+	{
+		.name = "modulated-pwm",
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(modulated_pwm_of_match),
+	},
+	.probe = modulated_pwm_probe,
+	.remove = modulated_pwm_remove,
+};
+
+module_platform_driver(modulated_pwm_driver);
+
+MODULE_DESCRIPTION("Modulated PWM");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:modulated-pwm");

Laerdal/linux-fslc

Note: For version 6.6 I had to change the Kconfig and Makefile.
And use devm_pwm_get instead of devm_of_pwm_get, since the latter has
been dropped, see the following commit:
    commit b88c48bfdd85820b19f0c0295a88d1596876e7c8
    Author: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Date:   Fri Aug 26 20:26:41 2022 +0300
        pwm: core: Get rid of unused devm_of_pwm_get()
        The devm_of_pwm_get() has recently lost its single user, drop
        the dead API as well.
        Note, the new code should use either plain pwm_get() or managed
        devm_pwm_get() or devm_fwnode_pwm_get() APIs.
And change  pwm_apply_state() to pwm_apply_might_sleep(), because of the
following commit:
    commit a10c3d5ff9a343c0b898215253750fedc38ac765
    Author: Sean Young <sean@mess.org>
    Date:   Tue Dec 19 16:30:24 2023 +0000
        pwm: Rename pwm_apply_state() to pwm_apply_might_sleep()
        [ Upstream commit c748a6d77c06a78651030e17da6beb278a1c9470 ]
Signed-off-by: Eino Juhani Oltedal <ejo@datarespons.no>

drivers/misc/dt-function-mux.c
@@ -1,0 +1,204 @@
+/*
+ * Simple PWM buzzer
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/pinctrl/consumer.h>
+
+
+#define MAX_HANDLES 2
+struct fmux_data {
+	struct device *dev;
+	const char* names[MAX_HANDLES];
+	int num_handles;
+	struct pinctrl *pinc;
+	struct pinctrl_state *pstate[MAX_HANDLES];
+	int active;
+};
+
+static DEFINE_MUTEX(sysfs_lock);
+
+
+static int fmux_reparse(struct fmux_data *priv)
+{
+	int ret;
+	int n;
+	ret = of_property_count_strings(priv->dev->of_node, "pinctrl-names");
+	if ( ret < 0 || ret > MAX_HANDLES) {
+		dev_err(priv->dev, "Wrong number of mux options or bad name string [%d]\n", ret);
+		return -EINVAL;
+	}
+
+	priv->num_handles = ret;
+	for (n=0; n < priv->num_handles; n++) {
+		ret = of_property_read_string_index(priv->dev->of_node, "pinctrl-names", n, &priv->names[n]);
+	}
+	return ret;
+}
+
+static int fmux_match(struct fmux_data *priv, const char* s)
+{
+	int n;
+	for (n=0; n < priv->num_handles; n++) {
+		if (strcmp(priv->names[n], s) == 0)
+			return n;
+	}
+	return -EINVAL;
+}
+
+static ssize_t fmux_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fmux_data *priv = dev_get_drvdata(dev);
+	ssize_t status=0;
+	mutex_lock(&sysfs_lock);
+	status = sprintf(buf, priv->names[priv->active]);
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static ssize_t fmux_opt_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fmux_data *priv = dev_get_drvdata(dev);
+	ssize_t status=0;
+	int n;
+	mutex_lock(&sysfs_lock);
+	for (n=0; n < priv->num_handles; n++)
+		status += sprintf(buf+status, "%s ", priv->names[n]);
+	status += sprintf(buf+status, "\n");
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static ssize_t fmux_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t sz)
+{
+	struct fmux_data *priv = dev_get_drvdata(dev);
+	size_t status = -EINVAL;
+	char sbuf[40];
+	int index;
+	mutex_lock(&sysfs_lock);
+	strncpy(sbuf, buf, min((int)sz, 39));
+	sbuf[sz] = '\0';
+
+	if ((index = fmux_match(priv, sbuf)) >= 0) {
+		if (index == priv->active)
+			status = sz;
+		else {
+			status = pinctrl_select_state(priv->pinc, priv->pstate[index]);
+			if (status < 0) {
+				dev_err(priv->dev, "Unable to select state %s\n", priv->names[index]);
+			}
+			else {
+				priv->active = index;
+				status = sz;
+			}
+		}
+	}
+	else {
+		dev_err(priv->dev, "Invalid selection %s\n", sbuf);
+	}
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static DEVICE_ATTR(select, 0644, fmux_show, fmux_store);
+static DEVICE_ATTR(possible, 0444, fmux_opt_show, NULL);
+
+
+static int fmux_probe(struct platform_device *pdev)
+{
+	struct fmux_data *priv;
+	int ret;
+	struct device_node *np = pdev->dev.of_node;
+
+	int status, n;
+
+	if (!np) {
+		dev_err(&pdev->dev, "No DT node found\n");
+		return -EINVAL;
+	}
+
+	priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv) {
+		dev_err(&pdev->dev, "no memory for state\n");
+		return -ENOMEM;
+	}
+
+	priv->dev = &pdev->dev;
+	platform_set_drvdata(pdev, priv);
+
+	fmux_reparse(priv);
+	if (priv->num_handles < 2) {
+		dev_err(priv->dev, "Too few [%d] handles\n", priv->num_handles);
+		status = -EINVAL;
+		goto deref;
+	}
+	priv->pinc = devm_pinctrl_get(priv->dev);
+	if ( IS_ERR(priv->pinc))
+	{
+		status = -ENODEV;
+		goto deref;
+	}
+	for (n=0; n < priv->num_handles; n++) {
+		priv->pstate[n] = pinctrl_lookup_state(priv->pinc, priv->names[n]);
+	}
+	mutex_lock(&sysfs_lock);
+	ret = pinctrl_select_state(priv->pinc, priv->pstate[0]);
+	if (ret < 0) {
+		dev_err(priv->dev, "Unable to select state %s\n", priv->names[0]);
+		status = ret;
+		mutex_unlock(&sysfs_lock);
+		goto deref;
+	}
+	priv->active = 0;
+	mutex_unlock(&sysfs_lock);
+
+	ret = sysfs_create_file(&pdev->dev.kobj, &dev_attr_select.attr);
+	ret = sysfs_create_file(&pdev->dev.kobj, &dev_attr_possible.attr);
+
+	return 0;
+deref:
+	return status;
+}
+
+static int fmux_remove(struct platform_device *pdev)
+{
+	sysfs_remove_file(&pdev->dev.kobj, &dev_attr_select.attr);
+	sysfs_remove_file(&pdev->dev.kobj, &dev_attr_possible.attr);
+	return 0;
+}
+
+static struct of_device_id fmux_of_match[] = {
+	{
+		.compatible = "datarespons,function-mux",
+	},
+	{}
+};
+
+MODULE_DEVICE_TABLE(of, fmux_of_match);
+
+static struct platform_driver fmux_driver = {
+	.driver =
+	{
+		.name = "function-mux",
+		.owner = THIS_MODULE,
+		.of_match_table = fmux_of_match,
+	},
+	.probe = fmux_probe,
+	.remove = fmux_remove,
+};
+
+module_platform_driver(fmux_driver);
+
+MODULE_DESCRIPTION("DT based function pinmux");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:function-mux");
+MODULE_AUTHOR("Hans Christian Lonstad <hcl@datarespons.no>");

drivers/misc/modulated_pwm.c
@@ -1,0 +1,147 @@
+/*
+ * Simple PWM buzzer
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/fb.h>
+#include <linux/backlight.h>
+#include <linux/err.h>
+#include <linux/pwm.h>
+#include <linux/slab.h>
+
+struct modulated_pwm_data {
+	struct pwm_device *pwm;
+	struct device *dev;
+	unsigned int pwmId;
+	struct pwm_state state;
+	int onValue;	/* dutycycle as onValue/256 */
+};
+
+static DEFINE_MUTEX(sysfs_lock);
+
+static ssize_t modulated_pwm_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+
+	struct modulated_pwm_data *pb = dev_get_drvdata(dev);
+	ssize_t status;
+	mutex_lock(&sysfs_lock);
+	status = sprintf(buf, "%d\n", pb->onValue);
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static ssize_t modulated_pwm_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t sz)
+{
+	struct modulated_pwm_data *pb = dev_get_drvdata(dev);
+	size_t status = sz;
+	int ret;
+	u8 val;
+
+	mutex_lock(&sysfs_lock);
+	ret = kstrtou8(buf, 10, &val);
+	if (ret < 0) {
+		status = ret;
+		goto exit;
+	}
+
+	pb->state.enabled = val == 0 ? false : true;
+	pwm_set_relative_duty_cycle(&pb->state, val, 255);
+
+	if ((ret = pwm_apply_might_sleep(pb->pwm, &pb->state)) < 0) {
+		dev_err(dev, "%s: Could not configure PWM", __func__);
+		status = ret;
+		goto exit;
+	} else {
+		pb->onValue = val;
+	}
+
+exit:
+	mutex_unlock(&sysfs_lock);
+	return status;
+}
+
+static DEVICE_ATTR(period, 0644, modulated_pwm_show, modulated_pwm_store);
+
+static int modulated_pwm_probe(struct platform_device *pdev)
+{
+	struct modulated_pwm_data *pb;
+	int ret;
+	struct device_node *np = pdev->dev.of_node;
+	u32 period;
+	bool inverted = of_property_read_bool(np, "inverted");
+	if (of_property_read_u32(np, "period", &period))
+		period = 0;
+	else
+		dev_info(&pdev->dev, "Got period of %d ns from DT\n", period);
+
+	pb = devm_kzalloc(&pdev->dev, sizeof(*pb), GFP_KERNEL);
+	if (!pb) {
+		dev_err(&pdev->dev, "no memory for state\n");
+		return -ENOMEM;
+	}
+
+	if (!np) {
+		dev_err(&pdev->dev, "No DT node found\n");
+		return -EINVAL;
+	}
+
+	pb->dev = &pdev->dev;
+	pb->onValue = 0;
+	pb->state.duty_cycle = 0;
+	pb->pwm = devm_pwm_get(&pdev->dev, NULL);
+	if (IS_ERR(pb->pwm)) {
+		dev_err(&pdev->dev, "unable to request PWM\n");
+		return PTR_ERR(pb->pwm);
+	}
+	pwm_init_state(pb->pwm, &pb->state);
+	dev_dbg(&pdev->dev, "%s: got pwm %d, period %llu ns, duty %llu ns\n", __func__, pb->pwmId,
+				pb->state.period, pb->state.duty_cycle);
+	pb->state.polarity = inverted ? PWM_POLARITY_INVERSED : PWM_POLARITY_NORMAL;
+	if (period)
+		pb->state.period = period;
+	pwm_apply_might_sleep(pb->pwm, &pb->state);
+
+	ret = sysfs_create_file(&pdev->dev.kobj, &dev_attr_period.attr);
+	platform_set_drvdata(pdev, pb);
+	return 0;
+}
+
+static int modulated_pwm_remove(struct platform_device *pdev)
+{
+	struct backlight_device *bl = platform_get_drvdata(pdev);
+	struct modulated_pwm_data *pb = dev_get_drvdata(&bl->dev);
+	sysfs_remove_file(&pdev->dev.kobj, &dev_attr_period.attr);
+	pwm_disable(pb->pwm);
+	return 0;
+}
+
+static struct of_device_id modulated_pwm_of_match[] = {
+	{
+		.compatible = "modulated-pwm",
+	},
+	{}
+};
+
+MODULE_DEVICE_TABLE(of, modulated_pwm_of_match);
+
+static struct platform_driver modulated_pwm_driver = {
+	.driver =
+	{
+		.name = "modulated-pwm",
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(modulated_pwm_of_match),
+	},
+	.probe = modulated_pwm_probe,
+	.remove = modulated_pwm_remove,
+};
+
+module_platform_driver(modulated_pwm_driver);
+
+MODULE_DESCRIPTION("Modulated PWM");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:modulated-pwm");